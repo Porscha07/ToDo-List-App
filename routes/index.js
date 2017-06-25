6c2c2a2d920b46e8f4d3995211bf235d301a490d
Porscha07 <porscha.uga@gmail.com>
2017-06-25 01:43:08 -0400

    register page is functional now

--- routes/index.js
+++ routes/index.js
@@ -84,57 +84,82 @@ router.post('/editItem',(req,res)=>{
     });
 });
 
+
+// ******************GETTING THE REGISTER PAGE**********************
+router.get('/register', function(req, res) {
+    res.render('register', {});
+});
+
+
+router.post('/register', (req,res)=>{
+
+    console.log(req.body.firstName);
+    
+    var firstName = req.body.firstName;
+    var lastName = req.body.lastName;
+    var userName = req.body.userName;
+    var email = req.body.Email;
+    var password = req.body.Password;
+    var confPassword = req.body.ConfPassword;
+    // var hash = bcrypt.hashSync(password);
+
+
+    var insertQuery = "INSERT INTO users (firstName, lastName, userName, Email, Password, ConfPassword ) VALUES (?,?,?,?,?,?)";
+
+    // res.send(insertQuery);
+    connection.query(insertQuery, [firstName, lastName, userName, email, password, confPassword],(error, results)=>{
+        if(error) throw error;
+        // console.log(results);
+        res.redirect('http://localhost:3000/?newaccount=created');
+
+    });
+
+
+});
+
+// ****************************************getting the  SIGN-IN page************************************
+
+router.get('/sign', function(req, res) {
+     var message = req.query.msg;
+         if(message == "badlogin"){
+            message="Incorrect Login"    
+        }else if (message == null){
+            message = " "
+        }
+    res.render('sign', {
+        message:message
+    });
+});
+
+
+router.post('/sign', function(req, res) {
+    var user = req.body.user;
+    var password = req.body.password;
+    var selectQuery = "SELECT userName, Password FROM users WHERE userName=? Password = ?)";
+    connection.query(selectQuery, [userName,Password],(error, results)=> {
+        if (results.length == 1) {
+            // var match = bcrypt.compareSync(password, results[0].password);
+            if (match == true) {
+                res.render('/localhost:3000');
+            }else {
+                res.redirect('/sign?msg=loginattempt=successful');
+            }
+        }else {
+            res.redirect('/sign?msg=badlogin');
+        }
+    })
+})
+
+
+
+
+
+//**************Logging Out********************
+// Log out
+// router.get('logout', function(req, res) {
+//     req.session.user = null;
+//     res.redirect('/sign');
+// });
+
+
 module.exports = router;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
