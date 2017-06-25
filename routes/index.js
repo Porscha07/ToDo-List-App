35af1efcb96873146ce11897ecadec0e5025430d
Porscha07 <porscha.uga@gmail.com>
2017-06-24 20:25:38 -0400

    have new bugs on sign in page and main page

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var ejs = require('ejs');
 var mysql = require('mysql');
-
 var connection = mysql.createConnection({
     host: '127.0.0.1',
     user: 'x',
@@ -44,13 +43,8 @@ router.post('/register', (req,res)=>{
 
 });
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+//****************************************getting the  SIGN-IN page************************************
 
-
-//getting the Home page-sign-in page
 router.get('/sign', function(req, res) {
      var message = req.query.msg;
          if(message == "badlogin"){
@@ -67,19 +61,66 @@ router.get('/sign', function(req, res) {
 router.post('/sign', function(req, res) {
     var user = req.body.user;
     var password = req.body.password;
-    var selectQuery = "SELECT * FROM Register WHERE user = ?";
-    connection.query(selectQuery, [user], function(error, results) {
+    var selectQuery = "SELECT userName, Password FROM Users WHERE userName=? Password = ?)";
+    connection.query(selectQuery, [userName,Password],(error, results)=> {
         if (results.length == 1) {
-            var match = bcrypt.compareSync(password, results[0].password);
+            // var match = bcrypt.compareSync(password, results[0].password);
             if (match == true) {
                 res.render('/localhost:3000');
             }else {
-                res.redirect('/sign?msg=badlogin');
+                res.redirect('/sign?msg=loginattempt=successful');
             }
         }else {
             res.redirect('/sign?msg=badlogin');
         }
     })
 })
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+
+// router.post('/', (req,res)=>{
+   
+//     var Monday = req.body.Monday;
+//     var Tuesday = req.body.Tuesday;
+//     var Wednesday = req.body.Wednesday;
+//     var Thursday = req.body.Thursday;
+//     var Friday = req.body.Friday;
+//     var AppointmentDetails = req.body.AppointmentDetails;
+//     var AppointmentTime = req.body.AppointmentTime;
+//     var Calls = req.body.Calls;
+//     var Emails = req.body.Wednesday;
+//     var CompleteList = req.body.CompleteList;
+//     var GetitDone = req.body.GetitDone;
+//     var Notes = req.body.Notes;
+//     // var hash = bcrypt.hashSync(password);
+
+
+//     var insertQuery = "INSERT INTO Tasks (Monday, Tuesday, Wednesday, Thursday, Friday , AppointmentDetails, AppointmentTime , Calls , Emails , CompleteList , GetitDone , Notes) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)";
+
+//     // res.send(insertQuery);
+//     connection.query(insertQuery, [Monday, Tuesday, Wednesday, Thursday, Friday , AppointmentDetails, AppointmentTime , Calls , Emails , CompleteList , GetitDone , Notes],(error, results)=>{
+//         if(error) throw error;
+//         console.log(results);
+//         res.redirect('http://localhost:3000/?toDoData=added');
+
+//     });
+
+
+// });
+
+
+//**************Logging Out********************
+// Log out
+// router.get('logout', function(req, res) {
+//     req.session.user = null;
+//     res.redirect('/sign');
+// });
+
 
 module.exports = router;
--- routes/scripts.js
+++ routes/scripts.js
@@ -1,5 +1,5 @@
 // $(document).ready(function() {
 //     $("#submit-login").click(function(){
-//         $('.index').show();
+//         $('Hello, <%=userName%>').show()
 //     });
 // });
