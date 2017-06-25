4dcac338f1e427a51202139e8cda6ff1c50b7f9d
Porscha07 <porscha.uga@gmail.com>
2017-06-25 01:02:09 -0400

    revision- new concept

--- routes/index.js
+++ routes/index.js
@@ -1,126 +1,140 @@
 var express = require('express');
 var router = express.Router();
-var ejs = require('ejs');
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'x',
-    password: 'x',
-    database: 'To-Do-App'
-});
+//import your custom "config" node module.
+//it hold a single object that has your MySQL credentials
+var config = require('../config/config');
 
-connection.connect();
-
-//******************GETTING THE REGISTER PAGE**********************
-router.get('/register', function(req, res) {
-    res.render('register', {});
-});
-
-
-router.post('/register', (req,res)=>{
-
-    console.log(req.body.firstName);
-    
-    var firstName = req.body.firstName;
-    var lastName = req.body.lastName;
-    var userName = req.body.userName;
-    var email = req.body.Email;
-    var password = req.body.Password;
-    var confPassword = req.body.ConfPassword;
-    // var hash = bcrypt.hashSync(password);
-
-
-    var insertQuery = "INSERT INTO Users (firstName, lastName, userName, Email, Password, ConfPassword ) VALUES (?,?,?,?,?,?)";
-
-    // res.send(insertQuery);
-    connection.query(insertQuery, [firstName, lastName, userName, email, password, confPassword],(error, results)=>{
-        if(error) throw error;
-        console.log(results);
-        res.redirect('http://localhost:3000/?newaccount=created');
-
+    //include your mysql module. we got this via npm
+    var mysql = require('mysql');// a way for mysql to talk to express
+    var connection = mysql.createConnection({
+        host: config.host,
+        user: config.userName,
+        password: config.password,
+        database: config.database
     });
-
-
-});
-
-//****************************************getting the  SIGN-IN page************************************
-
-router.get('/sign', function(req, res) {
-     var message = req.query.msg;
-         if(message == "badlogin"){
-            message="Incorrect Login"    
-        }else if (message == null){
-            message = " "
-        }
-    res.render('sign', {
-        message:message
-    });
-});
-
-
-router.post('/sign', function(req, res) {
-    var user = req.body.user;
-    var password = req.body.password;
-    var selectQuery = "SELECT userName, Password FROM Users WHERE userName=? Password = ?)";
-    connection.query(selectQuery, [userName,Password],(error, results)=> {
-        if (results.length == 1) {
-            // var match = bcrypt.compareSync(password, results[0].password);
-            if (match == true) {
-                res.render('/localhost:3000');
-            }else {
-                res.redirect('/sign?msg=loginattempt=successful');
-            }
-        }else {
-            res.redirect('/sign?msg=badlogin');
-        }
-    })
-})
-
+    //we are now connected ( after this line)!
+    connection.connect();// aka nodemon(run in terminal)
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    var message = req.query.msg;//can substitute msg for api key, etc
+    if(message == "added"){
+        message = "Your task was added!";
+    }else if(message == "updated!"){
+        message = "Your task was updated!"
+    }else if ( message == "deleted"){
+        message ="Your task was deleted!";
+    }
+    var selectQuery = "SELECT * FROM tasks";
+    //console.log("==========About to run query=========="");
+    connection.query(selectQuery,(error, results)=>{//query comes back with either error or results)
+    //console.log("====")
+        res.render('index',{
+            message: message,
+            taskArray: results// what we get back from running in mySql ( selectquery)
+        });
+    })
+});
+//add a post route "addItem to handle the form submission"
+//any form responses will be in req.body
+//**********How tasks are being added to the app and database*******************
+router.post('/addItem',(req,res)=>{
+    //res.json(req.body)
+    var newTask = req.body.newTask;
+    var dueDate = req.body.newTaskDate;
+    //we know what they submitted from the form. It comes from this route inside. req.body.NAMEOFFIELD. Now we need to insert it into MYSQL.
+    var insertQuery = "INSERT INTO tasks ( taskName, taskDate) VALUES('"+newTask+"','"+dueDate+"')";//insert statement
+    //res.send(insertQuery);//way to test your query
+    connection.query(insertQuery,(error,results)=>{
+        if(error) throw error;
+        res.redirect('/?msg=added');
+    });
+});
+//***********How tasks are being deleted from the app and database***************
+
+router.get('/delete/:id',(req,res)=>{
+    var idToDelete=req.params.id;
+    var deleteQuery ="DELETE from tasks WHERE id= " + idToDelete;
+    connection.query(deleteQuery,(error,results)=>{
+        res.redirect('/?msg=deleted')
+
+    });
+    //res.send(idToDelete);
+})
+//make a new route to handle the edit page. It will be /edit/IDTOEDIT
+router.get('/edit/:id',(req,res)=>{
+    var idToEdit = req.params.id;
+    var selectQuery = "SELECT * FROM tasks WHERE id = ?";
+    connection.query(selectQuery,[idToEdit],(error,results)=>{
+        res.render('edit',{
+            task: results[0]//only one item is returned
+        })
+        //res.json(results);//makes it easy to see what we get back
+
+    });
+});
+router.post('/editItem',(req,res)=>{
+    //res.json(req.body);
+    var newTask = req.body.newTask;//req.body pulled from forms
+    var newTaskDate = req.body.newTaskDate;
+    var idToEdit = req.query.id;//req.query- from query string.
+    var updateQuery = "UPDATE tasks SET taskName = ?, taskDate = ? WHERE id =?";
+    connection.query(update, [newTask, newTaskDate,idToEdit], (error,results)=>{
+        res.redirect('/?msg=updated');
+    });
 });
 
-
-
-// router.post('/', (req,res)=>{
-   
-//     var Monday = req.body.Monday;
-//     var Tuesday = req.body.Tuesday;
-//     var Wednesday = req.body.Wednesday;
-//     var Thursday = req.body.Thursday;
-//     var Friday = req.body.Friday;
-//     var AppointmentDetails = req.body.AppointmentDetails;
-//     var AppointmentTime = req.body.AppointmentTime;
-//     var Calls = req.body.Calls;
-//     var Emails = req.body.Wednesday;
-//     var CompleteList = req.body.CompleteList;
-//     var GetitDone = req.body.GetitDone;
-//     var Notes = req.body.Notes;
-//     // var hash = bcrypt.hashSync(password);
-
-
-//     var insertQuery = "INSERT INTO Tasks (Monday, Tuesday, Wednesday, Thursday, Friday , AppointmentDetails, AppointmentTime , Calls , Emails , CompleteList , GetitDone , Notes) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)";
-
-//     // res.send(insertQuery);
-//     connection.query(insertQuery, [Monday, Tuesday, Wednesday, Thursday, Friday , AppointmentDetails, AppointmentTime , Calls , Emails , CompleteList , GetitDone , Notes],(error, results)=>{
-//         if(error) throw error;
-//         console.log(results);
-//         res.redirect('http://localhost:3000/?toDoData=added');
-
-//     });
-
-
-// });
-
-
-//**************Logging Out********************
-// Log out
-// router.get('logout', function(req, res) {
-//     req.session.user = null;
-//     res.redirect('/sign');
-// });
-
-
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
